6422502c5e38ea228e32054b1ba38d753a1096c8
Lunark :3 <jvillaltamatos3007@gmail.com>
2025-12-28 23:30:56 -0500

    vermeer: Add Miuicamera if exist
    Change-Id: I6cc0c8276caeb9ec2ea8eb5d6435209ac3e6a44a
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- device.mk
+++ device.mk
@@ -18,6 +18,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/mixer_paths_kalama_mtp.xml \
     $(LOCAL_PATH)/configs/audio/resourcemanager_kalama_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_kalama/resourcemanager_kalama_mtp.xml
 
+# Camera
+$(call inherit-product-if-exists, vendor/xiaomi/vermeer-camera/miuicamera.mk)
+
 # Init
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init/init.vermeer.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.vermeer.rc
