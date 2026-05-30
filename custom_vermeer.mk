e637253df32d1adf23754c9337975ad5ff0153e6
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-05-30 14:30:05 -0500

    vermeer: PixelOs!
    Change-Id: Iddd9c2052ff16ee47d328f2989ac7848b9f603ab
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

--- custom_vermeer.mk
+++ custom_vermeer.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vermeer device
 $(call inherit-product, device/xiaomi/vermeer/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common PixelOs configuration
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := vermeer
-PRODUCT_NAME := lineage_vermeer
+PRODUCT_NAME := custom_vermeer
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := 23113RKC6G
 PRODUCT_MANUFACTURER := Xiaomi
